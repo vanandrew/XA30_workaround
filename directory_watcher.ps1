202c80223a53321be4f3d6b2c4f16094ba328bd8
Andrew Van <vanandrew@wustl.edu>
2023-05-26 02:07:39 -0500

    :sparkles: add fanciful text and colors

--- directory_watcher.ps1
+++ directory_watcher.ps1
@@ -1,6 +1,8 @@
 # specify the path to the folder you want to monitor:
-$MonitoringPath = "/home/vanandrew/Data/test"
-$global:DestinationPath = "/home/vanandrew/Data/test2"
+$MonitoringPath = "C:\ProgramData\Siemens\Numaris\SimMeasData\NORDIC"
+$global:DestinationPath = "Microsoft.PowerShell.Core\FileSystem::\\ARCHLEMUR\Data"
+# $MonitoringPath = "/home/vanandrew/Data/test"
+# $global:DestinationPath = "/home/vanandrew/Projects/XA30_workaround/shares/data"
 
 # specify which files you want to monitor
 $FileFilter = '*.dat'  
@@ -11,13 +13,12 @@ $IncludeSubfolders = $true
 # specify the file or folder properties you want to monitor:
 $AttributeFilter = [IO.NotifyFilters]::FileName, [IO.NotifyFilters]::LastWrite 
 
-try
-{
+try {
   $watcher = New-Object -TypeName System.IO.FileSystemWatcher -Property @{
-    Path = $MonitoringPath
-    Filter = $FileFilter
+    Path                  = $MonitoringPath
+    Filter                = $FileFilter
     IncludeSubdirectories = $IncludeSubfolders
-    NotifyFilter = $AttributeFilter
+    NotifyFilter          = $AttributeFilter
   }
 
   # define the code that should execute when a change occurs:
@@ -26,10 +27,10 @@ try
     
     # change type information:
     $details = $event.SourceEventArgs
-    $Name = $details.Name
+    # $Name = $details.Name
     $FullPath = $details.FullPath
-    $OldFullPath = $details.OldFullPath
-    $OldName = $details.OldName
+    # $OldFullPath = $details.OldFullPath
+    # $OldName = $details.OldName
     
     # type of change:
     $ChangeType = $details.ChangeType
@@ -50,46 +51,58 @@ try
     Write-Host ""
     Write-Host $text -ForegroundColor DarkYellow
 
-    # if the change type was Changed, copy the file over to the new directory at $NewPath
-    if ($ChangeType -eq 'Changed') {
+    # if the change type was Created, copy the file over to the new directory at $NewPath
+    if ($ChangeType -eq 'Created') {
       Write-Host "Copying $FullPath to $DestinationPath" -ForegroundColor DarkYellow
       Copy-Item $FullPath -Destination $DestinationPath
     }
-  }.GetNewClosure()
+  }
 
   # subscribe your event handler to all event types that are
   # important to you. Do this as a scriptblock so all returned
   # event handlers can be easily stored in $handlers:
   $handlers = . {
-    Register-ObjectEvent -InputObject $watcher -EventName Changed -Action $action 
+    # Register-ObjectEvent -InputObject $watcher -EventName Changed -Action $action 
     Register-ObjectEvent -InputObject $watcher -EventName Created -Action $action 
-    Register-ObjectEvent -InputObject $watcher -EventName Deleted -Action $action 
-    Register-ObjectEvent -InputObject $watcher -EventName Renamed -Action $action 
+    # Register-ObjectEvent -InputObject $watcher -EventName Deleted -Action $action 
+    # Register-ObjectEvent -InputObject $watcher -EventName Renamed -Action $action 
   }
 
   # monitoring starts now:
   $watcher.EnableRaisingEvents = $true
 
-  Write-Host "Watching for changes to $MonitoringPath"
+  Write-Host " _____  _               _                    __          __   _       _               "
+  Write-Host "|  __ \(_)             | |                   \ \        / /  | |     | |              "
+  Write-Host "| |  | |_ _ __ ___  ___| |_ ___  _ __ _   _   \ \  /\  / /_ _| |_ ___| |__   ___ _ __ "
+  Write-Host "| |  | | | '__/ _ \/ __| __/ _ \| '__| | | |   \ \/  \/ / _` | __/ __| '_ \ / _ \ '__|"
+  Write-Host "| |__| | | | |  __/ (__| || (_) | |  | |_| |    \  /\  / (_| | || (__| | | |  __/ |   "
+  Write-Host "|_____/|_|_|  \___|\___|\__\___/|_|   \__, |     \/  \/ \__,_|\__\___|_| |_|\___|_|   "
+  Write-Host "                                       __/ |                                          "
+  Write-Host "                                      |___/                                           "
+  Write-Host ""
 
   # since the FileSystemWatcher is no longer blocking PowerShell
   # we need a way to pause PowerShell while being responsive to
   # incoming events. Use an endless loop to keep PowerShell busy:
-  do
-  {
+  $symbols = @("( *    )", "(  *   )", "(   *  )", "(    * )", "(     *)", "(    * )", "(   *  )", "(  *   )",
+    "( *    )", "(*     )")
+  $i = 0;
+  do {
+    # Write symbol
+    $symbol = $symbols[$i]
+    Write-Host -NoNewLine "`r$symbol Watching for incoming *.dat files to $MonitoringPath" -ForegroundColor DarkCyan
     # Wait-Event waits for a second and stays responsive to events
-    # Start-Sleep in contrast would NOT work and ignore incoming events
     Wait-Event -Timeout 1
-
-    # write a dot to indicate we are still monitoring:
-    Write-Host "." -NoNewline
-        
+    # Increment to next symbol
+    $i++
+    if ($i -eq $symbols.Count) {
+      $i = 0;
+    }   
   } while ($true)
 }
-finally
-{
+finally {
   # this gets executed when user presses CTRL+C:
-  
+
   # stop monitoring
   $watcher.EnableRaisingEvents = $false
   
@@ -105,6 +118,7 @@ finally
   # properly dispose the FileSystemWatcher:
   $watcher.Dispose()
   
+  Write-Host ""
   Write-Warning "Event Handler disabled, monitoring ends."
 
   # clear DestinationPath variable
